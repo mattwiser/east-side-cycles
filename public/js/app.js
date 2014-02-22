848c7ef0062700cbab2cb57eec9ad54e584e28d6
Matthew Wiser <matt.k.wiser@gmail.com>
2014-02-22 17:49:50 -0600

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+/* global document, window, io */
+$(document).foundation();
+$(document).ready(initialize);
+
+function initialize(){
+  testing();
+};
+
+function testing(){
+  console.log('Hello World');
+};
