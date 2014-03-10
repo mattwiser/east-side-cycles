0c303d635efbd4975e4670529fc2aebe3a950e99
Matthew Wiser <matt.k.wiser@gmail.com>
2014-03-10 02:49:35 -0500

    slider fixed and navbar offset is finally done added image transitions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,8 @@ $(document).ready(initialize);
 
 function initialize(){
   set_width();
+  header_fade();
+  setInterval(header_fade,15000);
   var s = skrollr.init({
   render: function(data) {
         //Log the current scroll position.
@@ -16,4 +18,25 @@ function initialize(){
 
 function set_width(){
   $("row").css({"max-width":window.innerWidth});
+}
+
+
+function header_fade(){
+
+  var children = $("#parallax").children();
+  setTimeout(function(){
+    console.log("timeout 1")
+    $(children[1]).fadeOut(800);
+    $(children[2]).fadeIn(800);
+  }, 5000)
+    setTimeout(function(){
+    console.log("timeout 2")
+    $(children[2]).fadeOut(800);
+    $(children[0]).fadeIn(800);
+  }, 10000)
+    setTimeout(function(){
+    console.log("timeout 3")
+    $(children[0]).fadeOut(800);
+    $(children[1]).fadeIn(800);
+  }, 15000)
 }
