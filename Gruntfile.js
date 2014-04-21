c53d4cf51a8806465fe099cc81556b0714e22e77
Matthew Wiser <matt.k.wiser@gmail.com>
2014-04-21 02:34:37 -0500

    complete revamp of directory structure, custom grunt file, minor and major css/interface changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,333 +1,115 @@
-// Generated on 2014-03-05 using generator-jade 0.5.4
-'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to match all subfolders:
-// 'test/spec/**/*.js'
 
 module.exports = function(grunt) {
-  // load all grunt tasks
-  require('load-grunt-tasks')(grunt);
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
-
-  // configurable paths
-  var folders = {
-    app: 'app',
-    dist: 'dist',
-    tmp: '.tmp'
-  };
-
+  // configure the tasks
   grunt.initConfig({
-    folders: folders,
-    watch: {
-      compass: {
-        files: ['<%= folders.app %>/styles/{,*/}*.{scss,sass}'],
-        tasks: ['compass:server']
+
+    copy: {
+      build: {
+        cwd: 'dev',
+        src: [ '**/scripts/**', '**/img/**'],
+        dest: 'dist',
+        expand: true
       },
-      server: {
-        options: {
-          livereload: true
-        },
-        files: [
-          '<%= folders.tmp %>/*.html',
-          '<%= folders.tmp %>/styles/{,*/}*.css',
-          '{.tmp,<%= folders.app %>}/scripts/{,*/}*.js',
-          '<%= folders.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      },
-      jade: {
-        files: '<%= folders.app %>/jade/**/*.jade',
-        tasks: ['jade']
-      }
     },
-    connect: {
-      options: {
-        port: 9000,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost',
-        livereload: true
-      },
-      server: {
-        options: {
-          open: true,
-          base: [
-            '<%= folders.tmp %>',
-            '<%= folders.app %>'
-          ]
-        }
-      },
-      test: {
-        options: {
-          base: [
-            '<%= folders.tmp %>',
-            'test',
-            '<%= folders.app %>'
-          ]
-        }
-      },
-      dist: {
-        options: {
-          open: true,
-          base: [
-            '<%= folders.dist %>'
-          ],
-          livereload: false
-        }
-      }
-    },
+
     clean: {
+      build: {
+        src: [ 'dist' ]
+      },
+    },
+
+     jade: {
+      compile: {
+        options: {
+          data: {}
+        },
+        files: [{
+          expand: true,
+          cwd: 'dev/jade',
+          src: [ '!_*.jade', '*.jade' ],
+          dest: 'dist',
+          ext: '.html'
+        }]
+      }
+    },
+
+    sass: {
       dist: {
         files: [{
-          dot: true,
-          src: [
-            '<%= folders.tmp %>',
-            '<%= folders.dist %>/*',
-            '!<%= folders.dist %>/.git*'
-          ]
+          expand: true,
+          cwd: 'dev/sass',
+          src: ['**/*.scss', '!**/vendor/**'],
+          dest: 'dist/css',
+          ext: '.css'
         }]
-      },
-      server: '<%= folders.tmp %>'
-    },
-    mocha: {
-      all: {
-        options: {
-          run: true,
-          urls: ['http://localhost:<%= connect.options.port %>/index.html']
-        }
       }
     },
-    compass: {
-      options: {
-        sassDir: '<%= folders.app %>/styles',
-        cssDir: '<%= folders.tmp %>/styles',
-        imagesDir: '<%= folders.app %>/images',
-        javascriptsDir: '<%= folders.app %>/scripts',
-        fontsDir: '<%= folders.app %>/styles/fonts',
-        importPath: '<%= folders.app %>/bower_components',
-        relativeAssets: true
-      },
-      dist: {},
-      server: {
-        options: {
-          debugInfo: true
-        }
-      }
+
+  autoprefixer: {
+    build: {
+      expand: true,
+      cwd: 'dist',
+      src: [ '**/*.css' ],
+      dest: 'dist'
+    }
+  },
+
+  watch: {
+
+    stylesheets: {
+      files: 'dev/sass/main.scss',
+      tasks: [ 'stylesheets' ]
     },
     jade: {
-      html: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>/jade',
-          src: ['{,*/}*.jade', '!**/_*'],
-          dest: '.tmp/',
-          ext: '.html'
-        }],
-        options: {
-          client: false,
-          pretty: true,
-          basedir: '<%= folders.app %>/jade',
-          data: function(dest, src) {
-
-            var page = src[0].replace(/app\/jade\/(.*)\/index.jade/, '$1');
-
-            if (page == src[0]) {
-              page = 'index';
-            }
-
-            return {
-              page: page
-            };
-          }
-        }
-      }
-    },
-    rev: {
-      dist: {
-        files: {
-          src: [
-            '<%= folders.dist %>/scripts/{,*/}*.js',
-            '<%= folders.dist %>/styles/{,*/}*.css',
-            '<%= folders.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
-            '<%= folders.dist %>/styles/fonts/*'
-          ]
-        }
-      }
-    },
-    useminPrepare: {
-      html: '<%= folders.tmp %>/index.html',
-      options: {
-        dest: '<%= folders.dist %>'
-      }
-    },
-    usemin: {
-      html: ['<%= folders.dist %>/{,*/}*.html'],
-      css: ['<%= folders.dist %>/styles/{,*/}*.css'],
-      options: {
-        dirs: ['<%= folders.dist %>']
-      }
-    },
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>/images',
-          src: '{,*/}*.{png,jpg,jpeg}',
-          dest: '<%= folders.dist %>/images'
-        }]
-      }
-    },
-    svgmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>/images',
-          src: '{,*/}*.svg',
-          dest: '<%= folders.dist %>/images'
-        }]
-      }
-    },
-    cssmin: {
-      dist: {
-        files: {
-          '<%= folders.dist %>/styles/main.css': [
-            '<%= folders.tmp %>/styles/{,*/}*.css'
-          ]
-        }
-      }
-    },
-    htmlmin: {
-      dist: {
-        options: {
-          /*removeCommentsFromCDATA: true,
-          // https://github.com/folders/grunt-usemin/issues/44
-          //collapseWhitespace: true,
-          collapseBooleanAttributes: true,
-          removeAttributeQuotes: true,
-          removeRedundantAttributes: true,
-          useShortDoctype: true,
-          removeEmptyAttributes: true,
-          removeOptionalTags: true*/
-        },
-        files: [{
-          expand: true,
-          cwd: '<%= folders.tmp %>',
-          src: '{,*/}*.html',
-          dest: '<%= folders.dist %>'
-        }]
-      }
+      files: 'dev/**/*.jade',
+      tasks: [ 'jade' ]
     },
     copy: {
-      dist: {
-        files: [{
-          expand: true,
-          dot: true,
-          cwd: '<%= folders.app %>',
-          dest: '<%= folders.dist %>',
-          src: [
-            '*.{ico,txt}',
-            '.htaccess',
-            'images/{,*/}*.{webp,gif}',
-            'styles/fonts/*'
-          ]
-        }]
-      },
-      js: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>',
-          dest: '<%= folders.tmp %>',
-          src: [
-            'scripts/{,*/}*js', 'bower_components/**/*js'
-          ]
-        }]
-      },
-      css: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>',
-          dest: '<%= folders.tmp %>',
-          src: [
-            'styles/{,*/}*css'
-          ]
-        }]
-      },
-      assets: {
-        files: [{
-          expand: true,
-          cwd: '<%= folders.app %>',
-          dest: '<%= folders.dist %>',
-          src: [
-            'assets/{,*/}*.*'
-          ]
-        }]
-      }
-    },
-    concurrent: {
-      server: [
-        'compass:server'
-      ],
-      test: [
-        'compass'
-      ],
-      dist: [
-        'compass:dist',
-        'imagemin',
-        'svgmin',
-        'htmlmin'
-      ]
-    },
-    jshint: {
+      files: [ 'dev/**'],
+      tasks: [ 'copy' ]
+    }
+  },
+
+  connect: {
+    server: {
       options: {
-        reporter: require('jshint-stylish')
-      },
-      build: ['<%= folders.app %>/scripts/**/*js']
+        port: 9000,
+        base: 'dist',
+        hostname: 'localhost'
+      }
     }
+  }
+
   });
 
-  grunt.registerTask('server', function(target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'connect:dist:keepalive']);
-    }
 
-    grunt.task.run([
-      'clean:server',
-      'jade',
-      'concurrent:server',
-      'connect:server',
-      'watch'
-    ]);
-  });
+  // load the tasks
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('test', [
-    'clean:server',
-    'concurrent:test',
-    'connect:test',
-    'mocha'
-  ]);
+  // define the tasks
 
-  grunt.registerTask('build', [
-    'clean:dist',
-    'jade',
-    'copy:js',
-    'copy:css',
-    'useminPrepare',
-    'concurrent:dist',
-    'concat',
-    'cssmin',
-    'uglify',
-    'copy:dist',
-    'copy:assets',
-    'rev',
-    'usemin'
-  ]);
+  grunt.registerTask(
+    'stylesheets',
+    'Compiles the stylesheets.',
+    [ 'sass', 'autoprefixer' ]
+  );
 
-  grunt.registerTask('default', [
-    'jshint',
-    'test',
-    'build'
-  ]);
+  grunt.registerTask(
+    'build',
+    'Compiles all of the assets and copies the files to the dist directory.',
+    [ 'clean', 'jade', 'stylesheets', 'copy']
+  );
+
+  grunt.registerTask(
+    'default',
+    'Watches the project for changes, automatically builds them and runs a server.',
+    [ 'build', 'connect', 'watch' ]
+  );
 };
---
+++ dist/scripts/main.js
@@ -0,0 +1,186 @@
+/* global document, window, io */
+
+//global variables
+var s;
+var cnt;
+var isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+$(document).foundation({
+  orbit: {
+    animation: 'fade',
+    timer_speed: 2500,
+    slide_number: false,
+    animation_speed: 700,
+    pause_on_hover: false,
+    timer: false,
+    timer_progress_class: 'test',
+    bullets: false
+  }
+});
+$(document).ready(initialize);
+
+function initialize(){
+  skrollrInit();
+  skrollrToggle();
+  setWidth();
+  parallaxFade();
+  parallaxResize();
+  subContentResizeInitial();
+  $(window).on("resize", windowScroll);
+  $(window).on("resize", skrollrToggle);
+  $(window).on("resize", parallaxResize);
+  $(window).on("resize", subContentResize);
+  $(window).on("resize", interiorPageOffset);
+  $(window).triggerHandler('resize');
+  setInterval(parallaxFade,15000);
+}
+
+
+//===========================================================================================================//
+//===========================================================================================================//
+//===========================================================================================================//
+
+function skrollrInit(){
+  if (!isTouch) {
+    s = skrollr.init({
+      render: function(data) {
+        if (data.curTop>=350 ) {
+          $("nav").css("background-color", "rgba(0,0,0,1)")
+        };
+      },
+      forceHeight:false,
+      smoothScroll:true
+    });
+    if ($("#navHoist").length) {
+      window.onbeforeunload = function(e) {
+        window.scrollTo(0, 0);
+      };
+    };
+  };
+}
+
+function skrollrToggle(){
+
+  var mediaWidth = $(window).width() / parseFloat($("body").css("font-size"));
+
+
+  if (mediaWidth<64 && isTouch) {
+    $("nav").css("background-color", "black");
+  }
+  if (mediaWidth>64  && !s && !isTouch){
+      window.location.reload();
+  }
+}
+
+
+function windowScroll(){
+  if ($("#navHoist").length && !isTouch) {
+    window.scrollTo(0, 0);
+  };
+}
+
+function textResize(){
+  var text = $(".subContentText");
+  var textHeight = $(".subContentText").height();
+  $(text).css("font-size", textHeight * .2);
+}
+
+function subContentResizeInitial(){
+
+    $(window).load(function(){
+
+      var height = $(".subContentImage").height();
+      var width = $(".subContent").width();
+
+
+
+      $(".subContent").height(height);
+
+      $(".subContentColor1").height(height);
+      $(".subContentColor1").width(width);
+
+      $(".subContentColor2").height(height);
+      $(".subContentColor2").width(width);
+
+      $(".subContentColor3").height(height);
+      $(".subContentColor3").width(width);
+
+      $(".subContentColor4").height(height);
+      $(".subContentColor4").width(width);
+
+      textResize();
+
+  });
+};
+function subContentResize(){
+  var height = $(".subContentImage").height();
+  var width = $(".subContent").width();
+  var text = $(".subContentText");
+  var textHeight = $(".subContentText").height();
+
+
+  $(".subContent").height(height);
+
+
+  $(".subContentColor1").height(height);
+  $(".subContentColor1").width(width);
+
+  $(".subContentColor2").height(height);
+  $(".subContentColor2").width(width);
+
+  $(".subContentColor3").height(height);
+  $(".subContentColor3").width(width);
+
+  $(".subContentColor4").height(height);
+  $(".subContentColor4").width(width);
+
+  $(text).css("font-size", textHeight * .2);
+}
+
+function interiorPageOffset(){
+  var height = $("#header").height();
+  $("#body").css("top", height);
+}
+
+
+function setWidth(){
+  $("row").css({"max-width":window.innerWidth});
+}
+
+function parallaxResize(){
+
+  if ($("#headerImage1").height() != 0) {
+    var height = $("#headerImage1").height();
+  } else if($("#headerImage2").height() != 0){
+    var height = $("#headerImage2").height();
+  } else if($("#headerImage3").height() != 0){
+    var height = $("#headerImage3").height();
+  } else if (!height) {
+    $("#mainWrap").css("top", "24px");
+  }
+  // console.log(height);
+  $("#parallax_container").height(height);
+  if (height) {
+    $("#mainWrap").css("top", height + "px");
+  };
+
+}
+
+function parallaxFade(){
+
+  var children = $("#parallax").children();
+  setTimeout(function(){
+    // console.log("timeout 1");
+    $(children[1]).fadeOut(800);
+    $(children[2]).fadeIn(800);
+  }, 5000);
+    setTimeout(function(){
+    // console.log("timeout 2");
+    $(children[2]).fadeOut(800);
+    $(children[0]).fadeIn(800);
+  }, 10000);
+    setTimeout(function(){
+    // console.log("timeout 3");
+    $(children[0]).fadeOut(800);
+    $(children[1]).fadeIn(800);
+  }, 15000);
+}
